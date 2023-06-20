7d55df112bac05ca67f69dabdf41eb7c70c255bf
Martin Reinok <m.reinok@student.utwente.nl>
2023-06-21 01:33:07 +0200

    Added compile script

--- Assignment_11_rpi/build/CMakeFiles/20sim-pi.dir/xxinverse.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/20sim-pi.dir/xxinverse.c.o: \
- /mnt/c/Users/Cs/Desktop/Embedded\ Systems\ Laboratory/ESL201000168/Assignment_11_rpi/xxinverse.c \
- /usr/include/stdc-predef.h
---
+++ Assignment_11_rpi/compile.sh
@@ -0,0 +1,4 @@
+mkdir build && cd build
+cmake .. -DCMAKE_C_COMPILER=gcc
+make
+mv 20sim-pi ../20sim-pi
